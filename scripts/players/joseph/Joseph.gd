e2f4a2921ef54c94535354e501c0da433d554372
Guilherme <gui.crb0@gmail.com>
2022-10-02 15:32:52 -0300

    south animation

--- scripts/players/joseph/Joseph.gd
+++ scripts/players/joseph/Joseph.gd
@@ -1,38 +1,29 @@
-extends KinematicBody
+extends KinematicBody2D
 
-var motion = Vector3.ZERO
-export var speed = 1.0
-export var gravity = 0.2
+export var speed = 80
+export var gravity = 10
+
 onready var animationPlayer = $Animation/AnimationPlayer
 
+var motion = Vector2(0,0)
+
 func _process(delta):
-	
-	if(Input.is_action_pressed("up") and not Input.is_action_pressed("down")):
-		motion.z = -speed
-	elif (Input.is_action_pressed("down") and not Input.is_action_pressed("up")):
-		motion.z = speed
+	if Input.is_action_pressed("up") and not Input.is_action_pressed("down"):
+		motion.y = -speed/2
+	elif Input.is_action_pressed("down") and not Input.is_action_pressed("up"):
+		motion.y = speed/2
 	else:
-		 motion.z = 0
+		 motion.y = 0
 	
-	if(Input.is_action_pressed("left") and not Input.is_action_pressed("right")):
-		motion.x = -speed
-	elif(Input.is_action_pressed("right") and not Input.is_action_pressed("left")):
+	if Input.is_action_pressed("right") and not Input.is_action_pressed("left"):
 		motion.x = speed
+	elif Input.is_action_pressed("left") and not Input.is_action_pressed("right"):
+		motion.x = -speed
 	else:
 		motion.x = 0
-	
-	if(is_on_floor()):
-		motion.y = -1
-		if(motion.x != 0 or motion.z != 0):
-				animationPlayer.play("running_south")
-		else:
-				animationPlayer.play("idle_south")
-	else:
-		motion.y -= gravity
-		if(animationPlayer.is_playing()):
-			animationPlayer.play("falling_south")
 		
-	move_and_slide(motion, Vector3.UP)
-
-func _on_Area_body_entered(body):
-	translation = Vector3(0,0,0)
+	if motion.x == 0 && motion.y == 0:
+		animationPlayer.play("idle_south")
+	else:
+		animationPlayer.play("running_south")
+	move_and_slide(motion, Vector2(0, -1))
