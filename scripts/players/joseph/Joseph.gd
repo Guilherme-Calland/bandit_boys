9c91f808d5673b0355fe75255e146f76af29f93c
Guilherme <gui.crb0@gmail.com>
2022-11-10 12:28:43 -0300

    idle animations

--- scripts/players/joseph/Joseph.gd
+++ scripts/players/joseph/Joseph.gd
@@ -6,24 +6,115 @@ export var gravity = 10
 onready var animationPlayer = $Animation/AnimationPlayer
 
 var motion = Vector2(0,0)
+var direction = Directions.SOUTH
+enum Directions {
+	SOUTH,
+	SOUTHEAST,
+	EAST,
+	NORTHEAST,
+	NORTH,
+	NORTHWEST,
+	WEST,
+	SOUTHWEST
+}
+
+enum Buttons {
+	DOWN,
+	RIGHT,
+	UP,
+	LEFT
+}
+
+enum Animations {
+	IDLE,
+	RUNNING
+}
+
 
 func _process(delta):
-	if Input.is_action_pressed("up") and not Input.is_action_pressed("down"):
-		motion.y = -speed/2
-	elif Input.is_action_pressed("down") and not Input.is_action_pressed("up"):
+	## movement
+	
+	# vertical
+	if buttonPressed(Buttons.DOWN):
 		motion.y = speed/2
+	elif buttonPressed(Buttons.UP):
+		motion.y = -speed/2
 	else:
 		 motion.y = 0
 	
-	if Input.is_action_pressed("right") and not Input.is_action_pressed("left"):
+	# horizontal
+	if buttonPressed(Buttons.RIGHT):
 		motion.x = speed
-	elif Input.is_action_pressed("left") and not Input.is_action_pressed("right"):
+	elif buttonPressed(Buttons.LEFT):
 		motion.x = -speed
 	else:
 		motion.x = 0
-		
-	if motion.x == 0 && motion.y == 0:
-		animationPlayer.play("idle_south")
-	else:
-		animationPlayer.play("running_south")
 	move_and_slide(motion, Vector2(0, -1))
+	
+	## direction
+	if buttonPressed(Buttons.DOWN):
+		if buttonPressed(Buttons.RIGHT):
+			direction = Directions.SOUTHEAST
+		elif buttonPressed(Buttons.LEFT):
+			direction = Directions.SOUTHWEST
+		else:
+			direction = Directions.SOUTH
+	elif buttonPressed(Buttons.UP):
+		if buttonPressed(Buttons.RIGHT):
+			direction = Directions.NORTHEAST
+		elif buttonPressed(Buttons.LEFT):
+			direction = Directions.NORTHWEST
+		else: 
+			direction = Directions.NORTH
+	elif buttonPressed(Buttons.RIGHT):
+		direction = Directions.EAST
+	elif buttonPressed(Buttons.LEFT):
+		direction = Directions.WEST
+	
+	
+	## animation	
+	var animationKey
+	#if motion.x == 0 && motion.y == 0:
+	animationKey = Animations.IDLE
+	#else:
+		#animationParam = Animations.RUNNING
+	
+	animate(animationKey, direction)
+
+func animate(animationKey, directionKey):
+	var key1: String
+	var key2: String
+	if animationKey == Animations.IDLE:
+		key1 = "idle"
+	elif animationKey == Animations.RUNNING:
+		key1 = "running"
+	
+	if directionKey == Directions.SOUTH:
+		key2 = "south"
+	elif directionKey == Directions.SOUTHEAST:
+		key2 = "south_east"
+	elif directionKey == Directions.EAST:
+		key2 = "east"
+	elif directionKey == Directions.NORTHEAST:
+		key2 = "north_east"
+	elif directionKey == Directions.NORTH:
+		key2 = "north"
+	elif directionKey == Directions.NORTHWEST:
+		key2 = "north_west"
+	elif directionKey == Directions.WEST:
+		key2 = "west"
+	elif directionKey == Directions.SOUTHWEST:
+		key2 = "south_west"
+		
+	var joinedKey: String = key1 + "_" + key2
+	animationPlayer.play(joinedKey)
+
+func buttonPressed(button):
+	if button == Buttons.UP:
+		return Input.is_action_pressed("up") and not Input.is_action_pressed("down")
+	elif button == Buttons.DOWN:
+		return Input.is_action_pressed("down") and not Input.is_action_pressed("up")
+	elif button == Buttons.LEFT:
+		return Input.is_action_pressed("left") and not Input.is_action_pressed("right")
+	elif button == Buttons.RIGHT:
+		return Input.is_action_pressed("right") and not Input.is_action_pressed("left")
